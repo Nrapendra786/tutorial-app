6b0cf9108bf27f552a0fccc410d6a86cc0c03a6a
Nrapendra Trivedi <78588298+Nrapendra786@users.noreply.github.com>
2024-10-20 22:33:08 +0200

    Update clearAll.sh

--- clearAll.sh
+++ clearAll.sh
@@ -1,3 +1,9 @@
 docker-compose down --rmi all
 docker-compose down -v
 docker system prune --volumes
+docker stop $(docker ps -aq)                   # Stop all containers
+docker rm $(docker ps -aq)                     # Remove all containers
+docker rmi $(docker images -q)                  # Remove all images
+docker volume rm $(docker volume ls -q)        # Remove all volumes
+docker network rm $(docker network ls -q)      # Remove all custom networks
+docker system prune -a --volumes 

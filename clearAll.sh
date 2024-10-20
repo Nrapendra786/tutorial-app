7174958a14d48f56d65589d2b45dbf91489f5374
Nrapendra Trivedi <78588298+Nrapendra786@users.noreply.github.com>
2024-10-21 00:47:02 +0200

    Update clearAll.sh

--- clearAll.sh
+++ clearAll.sh
@@ -7,3 +7,5 @@ docker rmi $(docker images -q)                  # Remove all images
 docker volume rm $(docker volume ls -q)        # Remove all volumes
 docker network rm $(docker network ls -q)      # Remove all custom networks
 docker system prune -a --volumes 
+docker container prune && docker network prune && docker volume prune  #If you want remove everything but keep images
+
